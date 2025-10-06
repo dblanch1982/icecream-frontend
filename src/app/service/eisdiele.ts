349d9f2d6aea99da62f78d414dcbf86b679222b1
Daina Blanch <daina.blanch@gmail.com>
2025-10-06 18:05:06 +0200

    EisSorteListe ausgepasst

--- src/app/service/eisdiele.ts
+++ src/app/service/eisdiele.ts
@@ -2,25 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Eisdiele } from '../model/eisdiele.model';
+import { Eissorte } from '../model/eissorte.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EisdieleService {
 
-  // Beispielhafte lokale Daten (könnte auch eine API sein)
-  private eisdiele: Eisdiele = {
-    id: 1,
-    name: 'Eiszauber Altstadt Wasserburg',
-    adresse: 'Marienplatz 3, 83512 Wasserburg am Inn',
-    sorten: [
-      {
+  private eisSorteListe: any = [{
         name: 'Schokolade',
         beschreibung: 'Intensiv, cremig und voller Kakaogeschmack',
         preis: 1.5,
         vegan: false,
         zutaten: [{ name: 'Milch' }, { name: 'Kakao' }, { name: 'Zucker' }]
-      },
+    },
       {
         id: 2,
         name: 'Mango-Sorbet',
@@ -28,8 +23,14 @@ export class EisdieleService {
         preis: 1.8,
         vegan: true,
         zutaten: [{ name: 'Mango' }, { name: 'Zucker' }, { name: 'Wasser' }]
-      }
-    ]
+      }];
+
+  // Beispielhafte lokale Daten (könnte auch eine API sein)
+  private eisdiele: Eisdiele = {
+    id: 1,
+    name: 'Eiszauber Altstadt Wasserburg',
+    adresse: 'Marienplatz 3, 83512 Wasserburg am Inn',
+    sorten: [ this.eisSorteListe ]
   };
 
   constructor(private http: HttpClient) {}
